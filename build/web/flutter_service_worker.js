13392de50ca84a9e26a4b806daa3441b751579d3
Michael Rodrigues <myshelrodrigues@gmail.com>
2025-08-12 17:01:53 -0300

    Atualiza build web com ajustes de responsividade e scroll

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "3ec0926ee6c7df272c4990c183ddbb3a
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "ee9043fb5bd2cc80ec3ca8c7368266ff",
+"flutter_bootstrap.js": "13c6caf05e9a63b3551dba7b9e8c2a35",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
